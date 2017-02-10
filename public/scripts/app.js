3f25610dc1f603f6ccb9dc6a47611eb59d1458e7
ckc1230 <chriskchan1230@gmail.com>
2017-02-10 00:58:34 -0800

    lays out flashcards and reveals print button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,18 +5,28 @@ $(document).ready(function() {
 
 function processSubmission() {
   $('#flash-cards').empty();
-  var notes = $('#notes-input').val();
-  console.log(notes);
-  var questions = notes.split('~');
-  questions.forEach(function(question) {
-    var parts = question.split('|');
-    var prompt = parts[0] + '_____' + parts[2];
-    var promptDiv = $("<div>", {"class": "prompt"});
-    promptDiv.text(prompt);
-    var answerDiv = $("<div>", {"class": "answer"});
-    answerDiv.text(parts[1]);
+  if ($('#notes-input').val()) {
+    var notes = $('#notes-input').val();
+    console.log(notes);
+    var questions = notes.split('~');
+    questions.forEach(function(question, i) {
+      var parts = question.split('|');
+      var prompt = parts[0] + '_____' + parts[2];
+      var row = $("<div>", {"class": "row"});
+      var promptDiv = $("<div>", {"class": "card prompt"});
+      promptDiv.text(prompt);
+      var answerDiv = $("<div>", {"class": "card answer"});
+      answerDiv.text(parts[1]);
 
-    $('#flash-cards').append(promptDiv);
-    $('#flash-cards').append(answerDiv);
-  })
+      $('#flash-cards').append(row);
+      $('#flash-cards .row:nth-child('+(i+1)+')').append(promptDiv);
+      $('#flash-cards .row:nth-child('+(i+1)+')').append(answerDiv);
+    })
+    $('#print-button').css({"cursor": "pointer", "color": "black"});
+    $('#print-button').click(openPrint);
+  }
+}
+
+function openPrint() {
+  window.print();
 }
