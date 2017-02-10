0f2aa0994a64a518ed0db3c1869628b6c7e4b5a1
ckc1230 <chriskchan1230@gmail.com>
2017-02-10 14:19:54 -0800

    revises layout for page and print

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,40 @@
 $(document).ready(function() {
     console.log( "ready!" );
     $('#submit-button').click(processSubmission);
+    $('#show-button').click(toggleExample);
+    $('#hide-button').click(toggleExample);
 });
 
 function processSubmission() {
   $('#flash-cards').empty();
   if ($('#notes-input').val()) {
     var notes = $('#notes-input').val();
-    console.log(notes);
     var questions = notes.split('~');
     questions.forEach(function(question, i) {
       var parts = question.split('|');
       var prompt = parts[0] + '_____' + parts[2];
-      var row = $("<div>", {"class": "row"});
-      var promptDiv = $("<div>", {"class": "card prompt"});
+      var row = $("<tr>", {"class": "row"});
+      var promptDiv = $("<td>", {"class": "card prompt"});
       promptDiv.text(prompt);
-      var answerDiv = $("<div>", {"class": "card answer"});
+      var answerDiv = $("<td>", {"class": "card answer"});
       answerDiv.text(parts[1]);
 
       $('#flash-cards').append(row);
       $('#flash-cards .row:nth-child('+(i+1)+')').append(promptDiv);
       $('#flash-cards .row:nth-child('+(i+1)+')').append(answerDiv);
     })
+    $('html, body').animate({
+        scrollTop: $("#flash-cards").offset().top
+    }, 1000);
     $('#print-button').css({"cursor": "pointer", "color": "black"});
     $('#print-button').click(openPrint);
   }
+}
+
+function toggleExample() {
+  $('#card-example').toggle();
+  $('#hide-button').toggle();
+  $('#show-button').toggle();
 }
 
 function openPrint() {
